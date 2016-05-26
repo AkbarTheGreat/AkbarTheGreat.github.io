7d4f7c0300b808e8cb728de5cfd65dd46ea32be7
Tracy Beck <tgbeck@acm.org>
2016-05-25 21:56:56 -0400

    bluh

--- ak_worx.js
+++ ak_worx.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
     console.log( "ready!" );
     $("#worx_origin").change(function() {
+        console.log("Change");
         alert( "Handler for .change() called." );
     });
 });
