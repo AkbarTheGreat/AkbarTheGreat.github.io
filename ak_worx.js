1edf09d3f433c8dafc3bed736bc0e322d146d321
Tracy Beck <tgbeck@acm.org>
2016-05-26 00:48:56 -0400

    Bleh

--- ak_worx.js
+++ ak_worx.js
@@ -1,9 +1,22 @@
 
-$(document).ready(function() {
-    console.log( "ready!" );
-    $("#worx_origin").change(function() {
-        console.log("Change");
-        alert( "Handler for .change() called." );
-    });
+function getData(children)
+{
+	children.each(function(idx, val)
+	{
+		console.log("thing");
+		console.log( val.val() );
+	});
+}
+
+$(function()
+{
+	console.log( 'ready!' );
+	$('#go_button').click(function()
+	{
+//		$('#worx_origin').hide();
+//		getData($('#worx_origin #form4 input'));
+		innerDoc = $('#worx_origin')[0].contentDocument;
+		getData($('#form4', innerDoc));
+	});
 });
 
