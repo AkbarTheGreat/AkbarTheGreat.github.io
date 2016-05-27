4745ed7d04e4cb0b1c07544e3c37f59a68c57238
Tracy Beck <tgbeck@acm.org>
2016-05-27 08:07:58 -0400

    bluh

--- ak_worx.js
+++ ak_worx.js
@@ -1,4 +1,10 @@
 
+function getWorxDocument()
+{
+	var htmlString = $('#worx_origin').html();
+	return $('#worx_origin')[0].contentDocument;
+}
+
 function getData(children)
 {
 	children.each(function(idx, val)
@@ -10,12 +16,16 @@ function getData(children)
 
 $(function()
 {
+	   var script = document.createElement('script');
+	    script.setAttribute('type', 'text/javascript');
+	    script.setAttribute('src', 'http://www.worxnc.com/players/login1.php');
+
 	console.log( 'ready!' );
 	$('#go_button').click(function()
 	{
 //		$('#worx_origin').hide();
 //		getData($('#worx_origin #form4 input'));
-		innerDoc = $('#worx_origin')[0].contentDocument;
+		innerDoc = getWorxDocument();
 		getData($('#form4', innerDoc));
 	});
 });
